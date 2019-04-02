56c734b043af3c29227b44cbb80d9642889a2f36
Pierce Lauring <pclauring@gmail.com>
2019-04-02 14:44:48 -0400

    adding typography through config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
 import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default () => (
-  <div style={{ color: `black` }}>
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
     <Header />
-    Hello world!
+    <Link to="/about/">About</Link>
+    <br />
+    <Link to="/modules/">Modules</Link>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
   </div>
 )
